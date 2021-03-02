d4b6eefdf5ceb67ea58e54d4ac26621896cad0a0
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-02 05:25:05 +0000

    Add more queries

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(duration) as total_duration
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = student_id
+WHERE cohorts.name = 'FEB12';
